f5d9d1d3dd3098a5c85ce3eed56098102f6748ce
Rail Usmanov <rail.usmanov2055@gmail.com>
2024-06-06 15:18:21 +0300

    add counter Header

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -13,6 +13,7 @@ class Products {
             el.classList.remove(this.classNameActive);
             el.innerHTML = this.labelAdd;
         }
+        headerPage.render(products.length)
     }
     render(){
         const productsStore = localStorageUtil.getProducts();
